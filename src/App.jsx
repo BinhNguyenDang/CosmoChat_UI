2bb440cf0c1cccc85d641fdcdcf7b22159637869
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-07-12 23:50:01 +0700

    Chat component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing necessary components from React Router.
+import Chat from "./pages/Chat"; // Importing the Chat component.
 
+/**
+ * The root component of the application, responsible for defining routes using React Router.
+ * @returns {JSX.Element} The JSX element representing the entire application.
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* The `Routes` component is used to define the routing configuration */}
+      <Routes>
+        {/* Direct route for testing the Chat component */}
+        <Route path="/sessions/:id" element={<Chat />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default App; // Exporting the App component.
